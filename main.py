c593322e812bbef202e78f3553d0f5c164aa3d47
kurohei <97418587+kurohei@users.noreply.github.com>
2022-01-10 14:22:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(
+        debug = False,
+        host = '::'
+        port = 3182,
+        threaded = True
+    )
